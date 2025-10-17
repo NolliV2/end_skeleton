27a00bafe38274c0c9d43c5d37168afa178d4af6
NolliV2 <szkolny.patryk.c@gmail.com>
2025-10-17 10:47:22 +0200

    end_skeleton
    Skeleton

---
+++ end_skeleton.java
@@ -0,0 +1,52 @@
+// Made with Blockbench 5.0.1
+// Exported for Minecraft version 1.17+ for Yarn
+// Paste this class into your mod and generate all required imports
+public class end_skeleton extends EntityModel<Entity> {
+	private final ModelPart head;
+	private final ModelPart headwear;
+	private final ModelPart body;
+	private final ModelPart left_arm;
+	private final ModelPart right_arm;
+	private final ModelPart left_leg;
+	private final ModelPart right_leg;
+	public end_skeleton(ModelPart root) {
+		this.head = root.getChild("head");
+		this.headwear = root.getChild("headwear");
+		this.body = root.getChild("body");
+		this.left_arm = root.getChild("left_arm");
+		this.right_arm = root.getChild("right_arm");
+		this.left_leg = root.getChild("left_leg");
+		this.right_leg = root.getChild("right_leg");
+	}
+	public static TexturedModelData getTexturedModelData() {
+		ModelData modelData = new ModelData();
+		ModelPartData modelPartData = modelData.getRoot();
+		ModelPartData head = modelPartData.addChild("head", ModelPartBuilder.create().uv(0, 0).cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+
+		ModelPartData headwear = modelPartData.addChild("headwear", ModelPartBuilder.create().uv(32, 0).cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new Dilation(0.5F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+
+		ModelPartData body = modelPartData.addChild("body", ModelPartBuilder.create().uv(0, 16).cuboid(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+
+		ModelPartData left_arm = modelPartData.addChild("left_arm", ModelPartBuilder.create().uv(24, 18).cuboid(-1.0F, -2.0F, -1.0F, 2.0F, 12.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(5.0F, 2.0F, 0.0F));
+
+		ModelPartData right_arm = modelPartData.addChild("right_arm", ModelPartBuilder.create().uv(32, 18).cuboid(-1.0F, -2.0F, -1.0F, 2.0F, 12.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(-5.0F, 2.0F, 0.0F));
+
+		ModelPartData left_leg = modelPartData.addChild("left_leg", ModelPartBuilder.create().uv(40, 18).cuboid(-1.0F, 0.0F, -1.1F, 2.0F, 12.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(2.0F, 12.0F, 0.1F));
+
+		ModelPartData right_leg = modelPartData.addChild("right_leg", ModelPartBuilder.create().uv(48, 18).cuboid(-1.0F, 0.0F, -1.1F, 2.0F, 12.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(-2.0F, 12.0F, 0.1F));
+		return TexturedModelData.of(modelData, 64, 64);
+	}
+	@Override
+	public void setAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+	}
+	@Override
+	public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
+		head.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+		headwear.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+		body.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+		left_arm.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+		right_arm.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+		left_leg.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+		right_leg.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+	}
+}
